e2d85764ce3907e0628487a16f30b9b30554b0fe
LenaAQA <elenaqa94@gmail.com>
2026-03-08 18:08:11 +0300

    Integrating Allure into tests(suite)

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -21,12 +21,15 @@ from tools.assertions.users import assert_create_user_response, assert_get_user_
 @allure.tag(AllureTag.USERS, AllureTag.REGRESSION)
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.USERS)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.USERS)
 class TestUsers:
     @pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
     @allure.tag(AllureTag.CREATE_ENTITY)
     @allure.story(AllureStory.CREATE_ENTITY)
     @allure.title("Create user")
     @allure.severity(Severity.BLOCKER)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     def test_create_user(self, domain: str, public_users_client: PublicUsersClient):
         email = fake.email(domain=domain)
 
@@ -43,6 +46,7 @@ class TestUsers:
     @allure.story(AllureStory.GET_ENTITY)
     @allure.title("Get user me")
     @allure.severity(Severity.CRITICAL)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     def test_get_user_me(self, private_users_client: PrivateUsersClient, function_user):
         response = private_users_client.get_user_me_api()
         response_data = GetUserResponseSchema.model_validate_json(response.text)
